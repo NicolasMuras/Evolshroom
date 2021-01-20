e80ca92406bf00c632302ad443c1ffbee4b3362c
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-20 18:26:17 -0300

    Update builder_class.cpp
    Code updated, now we work with local values (Radius, X, Y)

--- source/builder_class.cpp
+++ source/builder_class.cpp
@@ -6,121 +6,26 @@
 #define PI 3.14159265358979324
 using namespace std;
 
-BuilderClass::BuilderClass(float &radio, float &X, float &Y)
+BuilderClass::BuilderClass(float radio, float X, float Y)
 {
+	setLocation(radio, X, Y);
 	setRandomCurvation();
 }
 
 void BuilderClass::setStage(int value)
 {
-	this->stage = value;
+	stage = value;
 }
 
 int BuilderClass::getStage() const {
 	return stage;
 }
 
-/*void BuilderClass::setLocation() 
+void BuilderClass::setLocation(float radio, float x, float y) 
 {
-	location[0] = 0;
-	location[1] = 0;
-}*/
-
-/*float* BuilderClass::getLocation() const {
-	return location;
-}
-
-float* BuilderClass::getCircle(int id) const {
-	return circles;
-}*/
-
-void BuilderClass::showMushroom() const
-{
-	
-	
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	for (int i = 0; i <= stage; ++i)
-	{
-		glBegin(GL_LINE_LOOP);
-		glColor3f(0.1 * this->stage, 0.0, 0.0);
-		for (int j = 0; j < 20; ++j)
-		{
-			glVertex3f(circle_group[i][j][0], circle_group[i][j][1], circle_group[i][j][2]);
-		}
-		glEnd();
-		glFlush();
-	}
-	
-	for (int k = 0; k < 12; ++k)
-	{
-		
-		glBegin(GL_LINE_LOOP);
-		glColor3f(0.0, 0.0, 0.1 * this->stage);
-		for (int j = 0; j < 20; ++j)
-		{
-			if (k > 11) {
-				continue;
-			}
-			glVertex3f(cap_group[k][j][0], cap_group[k][j][1], cap_group[k][j][2]);
-
-		}
-		glEnd();
-		glFlush();
-	}
-
-}
-
-void BuilderClass::translateCircle(int group)
-{
-	if (group == 1)
-	{
-		for (int i = 0; i < 12; ++i) // This move translate the cap in Z, this way we can simulate the grow movement
-		{
-			for (int j = 0; j < 20; ++j)
-			{
-				cap_group[i][j][2] += 0.1;
-			}
-		}
-	}
-	else if (group == 2) 
-	{
-		for (int i = 0; i < stage; ++i)
-		{
-			for (int j = 0; j < 20; ++j)
-			{
-				circle_group[i][j][2] += 0.1;
-			}
-		}
-	}
-	else 
-	{
-		std::cout << "Invalid group \n";
-	}
-
-}
-
-void BuilderClass::buildShroom(int &stage, float &Radio, float &X, float &Y)
-{
-	if (stage == 0)
-	{
-		for (int circle_stage = 0; circle_stage < 12; circle_stage++)
-		{
-			buildCapCircles(circle_stage, Radio, X, Y);
-		}
-		
-	}
-	if (stage < 23)
-	{
-		buildTrunk(stage, Radio, X, Y);
-		setStage(getStage() + 1);
-		translateCircle(1);
-	}
-	else 
-	{
-		std::cout << "No se puede agregar mas circulos\n";
-	}
-	
+	X_Loc = x;
+	Y_Loc = y;
+	Radio_Loc = radio;
 }
 
 void BuilderClass::setRandomCurvation()
@@ -156,12 +61,92 @@ float BuilderClass::getRandomFloat(int start, int end)
 	return uniform_dist(rng);
 }
 
-void BuilderClass::buildTrunk(int &stage, float &Radio, float &X, float &Y)
+void BuilderClass::showMushroom() const  // Draw the mushroom in the window
+{
+	for (int i = 0; i <= getStage(); ++i)
+	{
+		glBegin(GL_LINE_LOOP);
+		glColor3f(0.1 * getStage(), 0.0, 0.0);
+		for (int j = 0; j < 20; ++j)
+		{
+			glVertex3f(circle_group[i][j][0], circle_group[i][j][1], circle_group[i][j][2]);
+		}
+		glEnd();
+		glFlush();
+	}
+	
+	for (int k = 0; k < 12; ++k)
+	{
+		
+		glBegin(GL_LINE_LOOP);
+		glColor3f(0.0, 0.0, 0.1 * getStage());
+		for (int j = 0; j < 20; ++j)
+		{
+			if (k > 11) {
+				continue;
+			}
+			glVertex3f(cap_group[k][j][0], cap_group[k][j][1], cap_group[k][j][2]);
+
+		}
+		glEnd();
+		glFlush();
+	}
+}
+
+void BuilderClass::translateCircle(int group) // Translate the cap in Z axis
+{
+	if (group == 1)
+	{
+		for (int i = 0; i < 12; ++i) // This move translate the cap in Z, this way we can simulate the grow movement
+		{
+			for (int j = 0; j < 20; ++j)
+			{
+				cap_group[i][j][2] += 0.1;
+			}
+		}
+	}
+	else if (group == 2) 
+	{
+		for (int i = 0; i < getStage(); ++i)
+		{
+			for (int j = 0; j < 20; ++j)
+			{
+				circle_group[i][j][2] += 0.1;
+			}
+		}
+	}
+	else 
+	{
+		std::cout << "Invalid group \n";
+	}
+
+}
+
+void BuilderClass::buildShroom() // Build shrooms main function
+{
+	if (getStage() == 0)
+	{
+		for (int circle_stage = 0; circle_stage < 12; circle_stage++)
+		{
+			buildCapCircles(circle_stage);
+		}
+		
+	}
+	if (getStage() < 23)
+	{
+		buildTrunk();
+		setStage(getStage() + 1);
+		translateCircle(1);
+	}
+	
+}
+
+void BuilderClass::buildTrunk() // Set the curvature of the trunk and the gradual curvature of the circle radius.
 {
 	float Z = 0.1;
-	if (stage < 23)
+	if (getStage() < 23)
 	{
-		if (stage > curvation_start && stage < curvation_end) // Make curvation in the Y axis.
+		if (getStage() > curvation_start && getStage() < curvation_end) // Make curvation in the Y axis.
 		{
 			if (curvation_direction == 0) // Choose the direction
 			{
@@ -173,7 +158,7 @@ void BuilderClass::buildTrunk(int &stage, float &Radio, float &X, float &Y)
 			}
 		}
 
-		if (stage > curvation_end && stage < 23) // Reverse the curvation in the Y axis.
+		if (getStage() > curvation_end && getStage() < 23) // Reverse the curvation in the Y axis.
 		{
 			if (curvation_direction == 0) // Reverse direction.
 			{
@@ -185,22 +170,22 @@ void BuilderClass::buildTrunk(int &stage, float &Radio, float &X, float &Y)
 			}
 		}
 
-		if (stage < root_curvation_end) // Decremento gradual del radio en la raiz del hongo.
+		if (getStage() < root_curvation_end) // Decremento gradual del radio en la raiz del hongo.
 		{
 			trunk_curvation += 0.4 * PI / (root_curvation_end); // El "0.4" y el "8" ajusta la suavidad del decremento, es ideal dividir PI por la cantidad de circulos que se veran afectado, este caso son 8 circulos.
 		}
 
-		if (stage > trunk_curvation_start && stage < trunk_curvation_end) // Decremento gradual del radio 20 - 23
+		if (getStage() > trunk_curvation_start && getStage() < trunk_curvation_end) // Decremento gradual del radio 20 - 23
 		{
-			if (Radio * (cos(trunk_curvation)) > (Radio / 6))
+			if (Radio_Loc * (cos(trunk_curvation)) > (Radio_Loc / 6))
 			{
 				trunk_curvation += 0.1 * PI / (trunk_curvation_end - trunk_curvation_start);
 			}
 		}
 
-		float curved_radio = Radio * (cos(trunk_curvation));
-		float temp = Y + curved_radio * sin(circle_translation);
-		buildCircle(stage, curved_radio, X, temp, (Z * stage));
+		float curved_radio = Radio_Loc * (cos(trunk_curvation));
+		float curved_translation_y = Y_Loc + curved_radio * sin(circle_translation);
+		buildCircle(curved_radio, curved_translation_y, (Z * getStage()));
 	}
 	else 
 	{
@@ -208,7 +193,7 @@ void BuilderClass::buildTrunk(int &stage, float &Radio, float &X, float &Y)
 	}
 }
 
-void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, float Z)
+void BuilderClass::buildCircle(float Radio, float &curved_translation_y, float Z) // Build the circle with the current curved radios and translations.
 {
 
 	float t = 0; // Angle parameter.
@@ -217,7 +202,7 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 	float angle = 0;
 
 	static float rotation_var = 0;
-	if (stage > curvation_start && stage < curvation_end)
+	if (getStage() > curvation_start && getStage() < curvation_end)
 	{
 		if (curvation_direction == 0)
 		{
@@ -228,7 +213,7 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 		}
 		
 	}
-	if (stage > curvation_end && stage < 23)
+	if (getStage() > curvation_end && getStage() < 23)
 	{
 		if (curvation_direction == 0)
 		{
@@ -241,7 +226,7 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 
 	for (int i = 0; i < vertex_quantity; ++i)
 	{
-		if (stage > curvation_start && stage < curvation_end)
+		if (getStage() > curvation_start && getStage() < curvation_end)
 		{
 			inclination_y = (Radio) * (cos(circle_inclination));
 			inclination_z = (Radio) * (sin(circle_inclination));
@@ -257,7 +242,7 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 			}
 		}
 
-		if (stage > curvation_end && stage < 23) 
+		if (getStage() > curvation_end && getStage() < 23)
 		{
 			inclination_y = (Radio) * (cos(circle_inclination));
 			inclination_z = (Radio) * (sin(circle_inclination));
@@ -276,16 +261,16 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 		{
 			if (j == 0)
 			{
-				circle_group[stage][i][j] = X + Radio * cos(t);
+				circle_group[getStage()][i][j] = X_Loc + Radio * cos(t);
 			}
 			else if(j == 1)
 			{
-				circle_group[stage][i][j] = (Y + Radio * sin(t));
+				circle_group[getStage()][i][j] = (curved_translation_y + Radio * sin(t));
 			}
 			else if(j == 2)
 			{
 				
-				circle_group[stage][i][j] = Z + (inclination_z * rotation_var);
+				circle_group[getStage()][i][j] = Z + (inclination_z * rotation_var);
 			}
 
 		}
@@ -294,7 +279,7 @@ void BuilderClass::buildCircle(int &stage, float Radio, float &X, float &Y, floa
 	}
 }
 
-void BuilderClass::buildCapCircles(int &circle_stage, float &Radio, float &X, float &Y)
+void BuilderClass::buildCapCircles(int &circle_stage) // Build cap circles main function, set the radius curvature of the cap circles.
 {
 	float curved_radio;
 
@@ -303,18 +288,18 @@ void BuilderClass::buildCapCircles(int &circle_stage, float &Radio, float &X, fl
 	if (circle_stage == 0) // Decremento gradual del radio 1 - 8
 	{
 		cap_curvation += 0.5 * PI / 4; // El "0.4" y el "8" ajusta la suavidad del decremento
-		buildCapCircle(circle_stage, Radio / 2, X, Y, (Z * circle_stage));
+		buildCapCircle(circle_stage, Radio_Loc / 2, (Z * circle_stage));
 	}
 	if (circle_stage > 0 && circle_stage < 12) // Decremento gradual del radio 20 - 25
 	{
 		cap_curvation += 0.8 * PI / 11;
-		curved_radio = Radio * (sin(cap_curvation));
-		buildCapCircle(circle_stage, curved_radio, X, Y, (Z * circle_stage));
+		curved_radio = Radio_Loc * (sin(cap_curvation));
+		buildCapCircle(circle_stage, curved_radio, (Z * circle_stage));
 	}
 	
 }
 
-void BuilderClass::buildCapCircle(int &circle_stage, float Radio, float &X, float &Y, float Z)
+void BuilderClass::buildCapCircle(int &circle_stage, float Radio, float Z) // Build the current cap circle with the given parameters.
 {
 
 	float t = 0; // Angle parameter.
@@ -325,11 +310,11 @@ void BuilderClass::buildCapCircle(int &circle_stage, float Radio, float &X, floa
 		{
 			if (j == 0)
 			{
-				cap_group[circle_stage][i][j] = X + Radio * cos(t);
+				cap_group[circle_stage][i][j] = X_Loc + Radio * cos(t);
 			}
 			else if (j == 1)
 			{
-				cap_group[circle_stage][i][j] = Y + Radio * sin(t);
+				cap_group[circle_stage][i][j] = Y_Loc + Radio * sin(t);
 			}
 			else
 			{
@@ -341,7 +326,7 @@ void BuilderClass::buildCapCircle(int &circle_stage, float Radio, float &X, floa
 	}
 }
 
-void BuilderClass::buildCap(int &stage, float &Radio, float &X, float &Y, int &numVertices)
+void BuilderClass::buildCap(int &stage, float &Radio, float &X, float &Y, int &numVertices) // DON'T WORK
 {
 	float t = 0; // Angle parameter.
 	float t_90 = 0;
