0ef49344d77d6206320fb0fb74795b75bba25d2f
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-20 13:26:11 -0300

    Update builder_class.h

--- source/builder_class.h
+++ source/builder_class.h
@@ -7,7 +7,7 @@ class BuilderClass
 {
 public:
 	
-	BuilderClass(int &vertexquantity, float &radio, float &X, float &Y, int &y_segments);
+	BuilderClass(float &radio, float &X, float &Y);
 
 	void buildShroom(int &, float &, float &, float &);
 	void buildCap(int&, float&, float&, float&, int&);
@@ -15,16 +15,28 @@ public:
 	void buildCapCircles(int&, float&, float&, float&);
 	void buildCircle(int&, float, float&, float&, float);
 	void buildCapCircle(int&, float, float&, float&, float);
-	void deleteCircle();
-	void showTrunk() const;
+	int getRandomInt(int, int);
+	float getRandomFloat(int, int);
+	void showMushroom() const;
 
 	void setStage(int);
 	int getStage() const;
 
+	float circle_inclination = 0;
+	float circle_translation = 0;
 	float trunk_curvation = 0;
 	float cap_curvation = 0;
 	int stage = 0;
 	const int vertex_quantity = 20;
+
+	// Procedural vars
+	int curvation_start = 1;
+	int curvation_end = 23;
+	bool curvation_direction = 0;
+
+	int root_curvation_end = 5;
+	int trunk_curvation_start = 1;
+	int trunk_curvation_end = 23;
 private:
 	void translateCircle(int);
 	void scalateCircle();
@@ -32,7 +44,7 @@ private:
 
 	// SET
 	void setLocation();
-
+	void setRandomCurvation();
 
 	// GET
 	float* getLocation() const;
@@ -44,4 +56,3 @@ private:
 	float circle_group[24][20][3] = { 0 };
 };
 #endif // BUILDER_CLASS_H
-
