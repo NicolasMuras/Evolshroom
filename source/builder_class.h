55e0133e8cdcb1ad840cf35d6022d39ed8f5d2a1
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-21 14:50:41 -0300

    Update builder_class.h

--- source/builder_class.h
+++ source/builder_class.h
@@ -13,7 +13,7 @@ public:
 	void buildCap(int&, float&, float&, float&, int&);
 	void buildTrunk();
 	void buildCapCircles(int&);
-	void buildCircle(float, float&, float);
+	void buildCircle(float&, float);
 	void buildCapCircle(int&, float, float);
 	int getRandomInt(int, int);
 	float getRandomFloat(int, int);
@@ -33,12 +33,14 @@ public:
 	int curvation_start = 1;
 	int curvation_end = 23;
 	bool curvation_direction = 0;
+	float curvature_size = 1;
 
 	int root_curvation_end = 5;
 	int trunk_curvation_start = 1;
 	int trunk_curvation_end = 23;
 private:
-	void translateCircle(int);
+	void y_translateCap(float, int, float);
+	void z_inclinationCap(float, int);
 
 	// SET
 	void setLocation(float, float, float);
@@ -48,7 +50,9 @@ private:
 	float X_Loc = 0;
 	float Y_Loc = 0;
 	float Radio_Loc = 0;
+
 	float cap_group[12][20][3] = { 0 };
+	float cap_radius[12] = { 0 };
 	float circle_group[24][20][3] = { 0 };
 };
 #endif // BUILDER_CLASS_H
