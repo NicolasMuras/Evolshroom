2aea8db26d0c7cd42c158c64a29fe5f3bec7544e
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-23 02:43:32 -0300

    Update main.cpp
    Implemented VBO, VAO, now the shrooms have polygonal mesh, they grow in right way too, the mesh is updated in real time.

--- source/main.cpp
+++ source/main.cpp
@@ -3,13 +3,44 @@
 #include "builder_class.h"
 #include <random>
 #include <time.h>
+
 #define PI 3.14159265358979324
+
+#define VERTICES 0
+#define INDICES 1
+#define SHROOM 0
+#define CAP 1
+
+static unsigned int stripIndices[3000] = { 0 };
+static unsigned int stripIndicesCap[1500] = { 0 };
 // Globals.
 static float Xvalue = 0.0, Yvalue = 0.0; // Co-ordinates of the sphere.
 static float Angle = 0.0; // Angle to rotate the sphere.
 
+static unsigned int vao[2]; // Array of VAO ids.
+static unsigned int buffer[2]; // Array of buffer ids.
+
 Scene scene_1(1, 1);
 
+void fillIndices(std::vector<unsigned int> index, bool mode)
+{
+	
+	if (mode == 1)
+	{
+		for (int i = 0; i < index.size(); i++)
+		{
+			stripIndices[i] = index[i];
+		}
+	}
+	else if (mode == 0)
+	{
+		for (int i = 0; i < index.size(); i++)
+		{
+			stripIndicesCap[i] = index[i];
+		}
+	}
+
+}
 float getRandomFloat(float start, float end)
 {
 	std::mt19937 rng(time(NULL) * 15000);
@@ -17,28 +48,150 @@ float getRandomFloat(float start, float end)
 	return uniform_dist(rng);
 
 }
+
+void updateShrooms()
+{
+	for (int i = 0; i < scene_1.total_entities; ++i)
+	{
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES + i]);
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].circle_group) + sizeof(scene_1.Mushrooms_ID[i].colors_group), &scene_1.Mushrooms_ID[i].circle_group[0], GL_DYNAMIC_DRAW);
+
+		// Copy vertex coordinates data into first half of vertex buffer.
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].circle_group), scene_1.Mushrooms_ID[i].circle_group);
+
+		// Bind and fill indices buffer.
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES + i]);
+		fillIndices(scene_1.Mushrooms_ID[i].indices, 1);
+
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices), stripIndices, GL_DYNAMIC_DRAW);
+
+		// Enable two vertex arrays: co-ordinates and color.
+		glEnableClientState(GL_VERTEX_ARRAY);
+
+		// Specify vertex and color pointers to the start of the respective data.
+		glVertexPointer(3, GL_FLOAT, 0, 0);
+
+		glBindVertexArray(vao[SHROOM + i]);
+	}
+}
+
+void updateCaps()
+{
+	for (int i = 0; i < scene_1.Mushrooms_ID.size(); ++i)
+	{
+		// Bind vertex buffer and reserve space.
+		glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES + i]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].cap_group) + sizeof(scene_1.Mushrooms_ID[i].cap_colors), &scene_1.Mushrooms_ID[i].cap_group[0], GL_DYNAMIC_DRAW);
+
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].cap_group), scene_1.Mushrooms_ID[i].cap_group);
+
+		// Bind and fill indices buffer.
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES + i]);
+		fillIndices(scene_1.Mushrooms_ID[i].indices_cap, 0);
+
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap), stripIndicesCap, GL_DYNAMIC_DRAW);
+
+		// Enable two vertex arrays: co-ordinates and color.
+		glEnableClientState(GL_VERTEX_ARRAY);
+
+		glVertexPointer(3, GL_FLOAT, 0, 0);
+
+		glBindVertexArray(vao[CAP + i]);
+	}
+}
+
 // Drawing routine.
 void drawScene(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glLoadIdentity();
-	// Set the position of the sphere.
+	
 	glTranslatef(Xvalue, Yvalue, -5.0);
 	glRotatef(Angle, 1.0, 1.0, 1.0);
 
-	for (int i = 0; i < scene_1.Mushrooms_ID.size(); i++)
+	// Bind the vertex array oobject to set up our vertex buffers and index buffer
+	
+	updateShrooms();
+	glDrawElements(GL_TRIANGLES, 3000, GL_UNSIGNED_INT, 0);
+
+	updateCaps();
+	glDrawElements(GL_TRIANGLES, 1500, GL_UNSIGNED_INT, 0);
+	
+
+	// Shows the guides
+	/*for (int i = 0; i < scene_1.Mushrooms_ID.size(); i++)
 	{
 		scene_1.Mushrooms_ID[i].showMushroom();
-	}
+	}*/
 	
 	glutSwapBuffers();
+
 }
 
 // Initialization routine.
-void setup(void)
+void setup()
 {
+
 	glClearColor(0.5, 0.5, 0.5, 0.0);
+
+	glGenVertexArrays(2, vao); // Generate VAO ids.
+
+	// BEGIN bind VAO id vao[SHROOM] to the set of vertex array calls following.
+	glBindVertexArray(vao[SHROOM]);
+
+	glGenBuffers(2, buffer);
+
+	// Bind vertex buffer and reserve space.
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].circle_group) + sizeof(scene_1.Mushrooms_ID[0].colors_group), &scene_1.Mushrooms_ID[0].circle_group[0], GL_DYNAMIC_DRAW);
+
+	// Copy vertex coordinates data into first half of vertex buffer.
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[0].circle_group), scene_1.Mushrooms_ID[0].circle_group);
+
+	// Copy vertex color data into second half of vertex buffer.
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].circle_group), sizeof(scene_1.Mushrooms_ID[0].colors_group), scene_1.Mushrooms_ID[0].colors_group);
+
+	// Bind and fill indices buffer.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES]);
+	fillIndices(scene_1.Mushrooms_ID[0].indices, 1);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices), stripIndices, GL_DYNAMIC_DRAW);
+
+	// Enable two vertex arrays: co-ordinates and color.
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	// Specify vertex and color pointers to the start of the respective data.
+	glVertexPointer(3, GL_FLOAT, 0, 0);
+	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[0].circle_group)));
+	// END bind VAO id vao[SHROOM].
+	// -----------------------------------------------------------------------------------------------------------------------------------------------
+	// BEGIN bind VAO id vao[TRIANGLE] to the set of vertex array calls following.
+	glBindVertexArray(vao[CAP]);
+	glGenBuffers(2, buffer);
+
+	// Bind vertex buffer and reserve space.
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].cap_group) + sizeof(scene_1.Mushrooms_ID[0].cap_colors), &scene_1.Mushrooms_ID[0].cap_group[0], GL_DYNAMIC_DRAW);
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[0].cap_group), scene_1.Mushrooms_ID[0].cap_group);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].cap_group), sizeof(scene_1.Mushrooms_ID[0].cap_colors), scene_1.Mushrooms_ID[0].cap_colors);
+
+	// Bind and fill indices buffer.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES]);
+	fillIndices(scene_1.Mushrooms_ID[0].indices_cap, 0);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap), stripIndicesCap, GL_DYNAMIC_DRAW);
+
+	// Enable two vertex arrays: co-ordinates and color.
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, 0);
+	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[0].cap_group)));
 }
 
 // OpenGL window reshape routine.
@@ -49,6 +202,7 @@ void resize(int w, int h)
 	glLoadIdentity();
 	glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
 	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 // Keyboard input processing routine.
@@ -57,11 +211,16 @@ void keyInput(unsigned char key, int x, int y)
 	switch (key)
 	{
 	case '+':
-		scene_1.newMushroom(getRandomFloat(0.2, 0.7), getRandomFloat(-1.0, 1.0), getRandomFloat(-1.0, 1.0));
+		scene_1.newMushroom(getRandomFloat(0.2, 0.7), getRandomFloat(-1.0, 1.0), getRandomFloat(-1.0, 1.0), getRandomFloat(0.0, 0.3), getRandomFloat(0.0, 0.3), getRandomFloat(0.0, 0.3));
+		setup();
+		glutPostRedisplay();
+		break;
+	case 's':
+		scene_1.Mushrooms_ID[0].showIndices();
 		glutPostRedisplay();
 		break;
 	case 'p':
-		for (int i = 0; i < scene_1.Mushrooms_ID.size(); ++i)
+		for (int i = 0; i < scene_1.total_entities; ++i)
 		{
 			scene_1.Mushrooms_ID[i].buildShroom();
 		}
@@ -106,6 +265,7 @@ void printInteraction(void)
 // Main routine.
 int main(int argc, char **argv)
 {
+	Angle += 240.0;
 	printInteraction();
 	glutInit(&argc, argv);
 
@@ -126,7 +286,8 @@ int main(int argc, char **argv)
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	setup();
+	
 
 	glutMainLoop();
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------------------
