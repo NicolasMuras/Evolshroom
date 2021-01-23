320ddf8cbd63fafec7f45970300ccdfea995587d
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-23 02:40:51 -0300

    Update scene_class.h

--- source/scene_class.h
+++ source/scene_class.h
@@ -4,22 +4,28 @@
 #include "builder_class.h"
 #include <vector>
 
+#define VERTICES 0
+#define INDICES 1
+
 class Scene
 {
 	
 public:
 	Scene(float, float);
 
-	void newMushroom(float, float, float);
+	void newMushroom(float, float, float, float, float, float);
 	
 	// SET
 	void setArea(float, float);
 
 	// VARS
 	std::vector<BuilderClass> Mushrooms_ID;
+	
+	int total_entities = 0;
 
 private:
 	// VARS
+	
 	float area_x = 0;
 	float area_y = 0;
 };
