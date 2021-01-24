27871cf32649210d30349da3d451dd0f4334d2cb
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-24 01:06:08 -0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 #include "builder_class.h"
 #include <random>
 #include <time.h>
+#include "SOIL2.h"
 
 #define PI 3.14159265358979324
 
@@ -11,155 +12,293 @@
 #define SHROOM 0
 #define CAP 1
 
-static unsigned int stripIndices[3000] = { 0 };
-static unsigned int stripIndicesCap[1500] = { 0 };
+#define NUMBERPRIMITIVES 3
 // Globals.
+
 static float Xvalue = 0.0, Yvalue = 0.0; // Co-ordinates of the sphere.
 static float Angle = 0.0; // Angle to rotate the sphere.
+static char guidesMode = 'a';
+static char HUDtex;
+static int width, height; // OpenGL window size.
 
-static unsigned int vao[2]; // Array of VAO ids.
-static unsigned int buffer[2]; // Array of buffer ids.
+static unsigned int stripIndices[15][3000] = { 0 };
+static unsigned int stripIndicesCap[15][1500] = { 0 };
+static unsigned int vao[15][2]; // Array of VAO ids.
+static unsigned int buffer[15][2]; // Array of buffer ids.
 
 Scene scene_1(1, 1);
+GLuint textures[10] = {1};
 
-void fillIndices(std::vector<unsigned int> index, bool mode)
+void writeStrokeString(void *font, char *string)
+{
+	char *c;
+	for (c = string; *c != '\0'; c++)
+	{
+		glutBitmapCharacter(font, *c);
+	}
+}
+void drawText()
+{
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glWindowPos2i(30, 112);
+	writeStrokeString(GLUT_BITMAP_HELVETICA_18, (char *)"Psilocybe\tCubensis");
+	glColor3f(0.7f, 0.2f, 0.1f);
+	glWindowPos2i(30, 87);
+	writeStrokeString(GLUT_BITMAP_HELVETICA_18, (char *)"B+");
+}
+GLuint textureInit(const char* image_path)
+{
+	// TEXTURE INIT
+	int image_width = 0;
+	int image_height = 0;
+
+	unsigned char* image = SOIL_load_image(image_path, &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
+
+	GLuint texture0;
+	glGenTextures(1, &texture0);
+	glBindTexture(GL_TEXTURE_2D, texture0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << "\n";
+	}
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image);
+
+	return texture0;
+}
+void drawStars(int calification)
+{
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[1]);
+	for (int i = 0; i < calification; i++)
+	{
+		glBegin(GL_QUADS);
+		glColor3f(255, 255, 255);
+		glTexCoord2f(0, 1);
+		glVertex2f(100 + (i * 50), 10);
+		glTexCoord2f(0, 0);
+		glVertex2f(100 + (i * 50), 50);
+		glTexCoord2f(1, 0);
+		glVertex2f(140 + (i * 50), 50);
+		glTexCoord2f(1, 1);
+		glVertex2f(140 + (i * 50), 10);
+		glTexCoord2f(0, 1);
+		glEnd();
+	}
+}
+void drawSelectionSquare(int selected_id)
+{
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[2]);
+	for (int i = 0; i < selected_id; i++)
+	{
+		if (i == selected_id - 1)
+		{
+			glBegin(GL_QUADS);
+				glColor3f(255, 255, 255);
+				glTexCoord2f(0, 1);
+				glVertex2f(370 + (i * 48), 99);
+				glTexCoord2f(0, 0);
+				glVertex2f(370 + (i * 48), 137);
+				glTexCoord2f(1, 0);
+				glVertex2f(411 + (i * 48), 137);
+				glTexCoord2f(1, 1);
+				glVertex2f(411 + (i * 48), 99);
+				glTexCoord2f(0, 1);
+			glEnd();
+		}
+	}
+}
+void drawHUD(void)
+{
+	glDisable(GL_CULL_FACE);
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glDisable(GL_DEPTH_TEST);
+	glDepthMask(GL_FALSE);
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, 1000, 0, 1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	if (textures[0] == 1)  // Load textures once time
+	{
+		textures[0] = textureInit("hud/hud_old.png");
+		textures[1] = textureInit("hud/star.png");
+		textures[2] = textureInit("hud/selection_square.png");
+		textures[3] = textureInit("hud/colored_shroom.png");
+		textures[4] = textureInit("hud/gray_shroom.png");
+	}
+
+	glEnable(GL_TEXTURE_2D);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+	glEnable(GL_BLEND);
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.1);
+
+	glBegin(GL_QUADS);
+		glColor3f(255, 255, 255);
+		glTexCoord2f(0, 1);
+		glVertex2f(0, 0);
+		glTexCoord2f(0, 0);
+		glVertex2f(0, 150);
+		glTexCoord2f(1, 0);
+		glVertex2f(1000, 150);
+		glTexCoord2f(1, 1);
+		glVertex2f(1000, 0);
+		glTexCoord2f(0, 1);
+		
+	glEnd();
+
+	drawStars(5);
+	drawSelectionSquare(1);
+
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_BLEND);
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+}
+void showGuides()
+{
+	if (guidesMode == 'a')
+	{
+		// Shows the guides
+		for (int i = 0; i < scene_1.Mushrooms_ID.size(); i++)
+		{
+			scene_1.Mushrooms_ID[i].showMushroom();
+		}
+	}
+
+}
+void fillIndices(std::vector<unsigned int> index, bool mode, int array_id)
 {
 	
 	if (mode == 1)
 	{
 		for (int i = 0; i < index.size(); i++)
 		{
-			stripIndices[i] = index[i];
+			stripIndices[array_id][i] = index[i];
 		}
 	}
 	else if (mode == 0)
 	{
 		for (int i = 0; i < index.size(); i++)
 		{
-			stripIndicesCap[i] = index[i];
+			stripIndicesCap[array_id][i] = index[i];
 		}
 	}
 
 }
 float getRandomFloat(float start, float end)
 {
-	std::mt19937 rng(time(NULL) * 15000);
+	std::random_device r;
+	std::seed_seq seed{ r(), r(), r(), r(), r(), r(), r(), r() };
+
+	std::mt19937 rng(seed);
 	std::uniform_real_distribution<float> uniform_dist(start, end);
 	return uniform_dist(rng);
 
 }
-
 void updateShrooms()
 {
-	for (int i = 0; i < scene_1.total_entities; ++i)
+	for (int i = 0; i < scene_1.Mushrooms_ID.size(); i++)
 	{
 
-		glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES + i]);
+		glBindVertexArray(vao[i][SHROOM]);
 
+		glBindBuffer(GL_ARRAY_BUFFER, buffer[i][VERTICES]);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].circle_group) + sizeof(scene_1.Mushrooms_ID[i].colors_group), &scene_1.Mushrooms_ID[i].circle_group[0], GL_DYNAMIC_DRAW);
-
-		// Copy vertex coordinates data into first half of vertex buffer.
 		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].circle_group), scene_1.Mushrooms_ID[i].circle_group);
-
-		// Bind and fill indices buffer.
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES + i]);
-		fillIndices(scene_1.Mushrooms_ID[i].indices, 1);
-
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices), stripIndices, GL_DYNAMIC_DRAW);
-
-		// Enable two vertex arrays: co-ordinates and color.
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[i][INDICES]);
+		fillIndices(scene_1.Mushrooms_ID[i].indices, 1, i);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices[i]), stripIndices[i], GL_DYNAMIC_DRAW);
 		glEnableClientState(GL_VERTEX_ARRAY);
-
-		// Specify vertex and color pointers to the start of the respective data.
 		glVertexPointer(3, GL_FLOAT, 0, 0);
 
-		glBindVertexArray(vao[SHROOM + i]);
-	}
-}
+		glDrawElements(GL_TRIANGLES, 3000, GL_UNSIGNED_INT, 0);
 
-void updateCaps()
-{
-	for (int i = 0; i < scene_1.Mushrooms_ID.size(); ++i)
-	{
-		// Bind vertex buffer and reserve space.
-		glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES + i]);
+
+
+		glBindVertexArray(vao[i][CAP]);
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffer[i][VERTICES]);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].cap_group) + sizeof(scene_1.Mushrooms_ID[i].cap_colors), &scene_1.Mushrooms_ID[i].cap_group[0], GL_DYNAMIC_DRAW);
-
 		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].cap_group), scene_1.Mushrooms_ID[i].cap_group);
-
-		// Bind and fill indices buffer.
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES + i]);
-		fillIndices(scene_1.Mushrooms_ID[i].indices_cap, 0);
-
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap), stripIndicesCap, GL_DYNAMIC_DRAW);
-
-		// Enable two vertex arrays: co-ordinates and color.
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[i][INDICES]);
+		fillIndices(scene_1.Mushrooms_ID[i].indices_cap, 0, i);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap[i]), stripIndicesCap[i], GL_DYNAMIC_DRAW);
 		glEnableClientState(GL_VERTEX_ARRAY);
-
 		glVertexPointer(3, GL_FLOAT, 0, 0);
 
-		glBindVertexArray(vao[CAP + i]);
+		glDrawElements(GL_TRIANGLES, 1500, GL_UNSIGNED_INT, 0);
+		
 	}
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
 	glLoadIdentity();
 	
 	glTranslatef(Xvalue, Yvalue, -5.0);
 	glRotatef(Angle, 1.0, 1.0, 1.0);
 
-	// Bind the vertex array oobject to set up our vertex buffers and index buffer
 	
 	updateShrooms();
-	glDrawElements(GL_TRIANGLES, 3000, GL_UNSIGNED_INT, 0);
+	showGuides();
+	drawHUD();
+	drawText();
 
-	updateCaps();
-	glDrawElements(GL_TRIANGLES, 1500, GL_UNSIGNED_INT, 0);
-	
 
-	// Shows the guides
-	/*for (int i = 0; i < scene_1.Mushrooms_ID.size(); i++)
-	{
-		scene_1.Mushrooms_ID[i].showMushroom();
-	}*/
-	
+
 	glutSwapBuffers();
-
+	glutPostRedisplay();
 }
 
 // Initialization routine.
-void setup()
+void setup(int i)
 {
-
-	glClearColor(0.5, 0.5, 0.5, 0.0);
-
-	glGenVertexArrays(2, vao); // Generate VAO ids.
+	glGenVertexArrays(2, vao[i]); // Generate VAO ids.
 
 	// BEGIN bind VAO id vao[SHROOM] to the set of vertex array calls following.
-	glBindVertexArray(vao[SHROOM]);
+	glBindVertexArray(vao[i][SHROOM]);
 
-	glGenBuffers(2, buffer);
+	glGenBuffers(2, buffer[i]);
 
 	// Bind vertex buffer and reserve space.
-	glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES]);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[i][VERTICES]);
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].circle_group) + sizeof(scene_1.Mushrooms_ID[0].colors_group), &scene_1.Mushrooms_ID[0].circle_group[0], GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].circle_group) + sizeof(scene_1.Mushrooms_ID[i].colors_group), &scene_1.Mushrooms_ID[i].circle_group[0], GL_DYNAMIC_DRAW);
 
 	// Copy vertex coordinates data into first half of vertex buffer.
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[0].circle_group), scene_1.Mushrooms_ID[0].circle_group);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].circle_group), scene_1.Mushrooms_ID[i].circle_group);
 
 	// Copy vertex color data into second half of vertex buffer.
-	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].circle_group), sizeof(scene_1.Mushrooms_ID[0].colors_group), scene_1.Mushrooms_ID[0].colors_group);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].circle_group), sizeof(scene_1.Mushrooms_ID[i].colors_group), scene_1.Mushrooms_ID[i].colors_group);
 
 	// Bind and fill indices buffer.
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES]);
-	fillIndices(scene_1.Mushrooms_ID[0].indices, 1);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[i][INDICES]);
+	fillIndices(scene_1.Mushrooms_ID[i].indices, 1, i);
 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices), stripIndices, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndices[i]), stripIndices[i], GL_DYNAMIC_DRAW);
 
 	// Enable two vertex arrays: co-ordinates and color.
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -167,31 +306,32 @@ void setup()
 
 	// Specify vertex and color pointers to the start of the respective data.
 	glVertexPointer(3, GL_FLOAT, 0, 0);
-	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[0].circle_group)));
+	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[i].circle_group)));
 	// END bind VAO id vao[SHROOM].
+
 	// -----------------------------------------------------------------------------------------------------------------------------------------------
 	// BEGIN bind VAO id vao[TRIANGLE] to the set of vertex array calls following.
-	glBindVertexArray(vao[CAP]);
-	glGenBuffers(2, buffer);
+	glBindVertexArray(vao[i][CAP]);
+	glGenBuffers(2, buffer[i]);
 
 	// Bind vertex buffer and reserve space.
-	glBindBuffer(GL_ARRAY_BUFFER, buffer[VERTICES]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].cap_group) + sizeof(scene_1.Mushrooms_ID[0].cap_colors), &scene_1.Mushrooms_ID[0].cap_group[0], GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[i][VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].cap_group) + sizeof(scene_1.Mushrooms_ID[i].cap_colors), &scene_1.Mushrooms_ID[i].cap_group[0], GL_DYNAMIC_DRAW);
 
-	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[0].cap_group), scene_1.Mushrooms_ID[0].cap_group);
-	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[0].cap_group), sizeof(scene_1.Mushrooms_ID[0].cap_colors), scene_1.Mushrooms_ID[0].cap_colors);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(scene_1.Mushrooms_ID[i].cap_group), scene_1.Mushrooms_ID[i].cap_group);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(scene_1.Mushrooms_ID[i].cap_group), sizeof(scene_1.Mushrooms_ID[i].cap_colors), scene_1.Mushrooms_ID[i].cap_colors);
 
 	// Bind and fill indices buffer.
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[INDICES]);
-	fillIndices(scene_1.Mushrooms_ID[0].indices_cap, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer[i][INDICES]);
+	fillIndices(scene_1.Mushrooms_ID[i].indices_cap, 0, i);
 
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap), stripIndicesCap, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(stripIndicesCap[i]), stripIndicesCap[i], GL_DYNAMIC_DRAW);
 
 	// Enable two vertex arrays: co-ordinates and color.
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
 	glVertexPointer(3, GL_FLOAT, 0, 0);
-	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[0].cap_group)));
+	glColorPointer(3, GL_FLOAT, 0, (void *)(sizeof(scene_1.Mushrooms_ID[i].cap_group)));
 }
 
 // OpenGL window reshape routine.
@@ -211,12 +351,26 @@ void keyInput(unsigned char key, int x, int y)
 	switch (key)
 	{
 	case '+':
-		scene_1.newMushroom(getRandomFloat(0.2, 0.7), getRandomFloat(-1.0, 1.0), getRandomFloat(-1.0, 1.0), getRandomFloat(0.0, 0.3), getRandomFloat(0.0, 0.3), getRandomFloat(0.0, 0.3));
-		setup();
+		//scene_1.newMushroom(getRandomFloat(0.2, 0.7), getRandomFloat(-2.0, 2.0), getRandomFloat(-2.0, 2.0), getRandomFloat(0.005, 0.03), getRandomFloat(0.005, 0.03), getRandomFloat(0.005, 0.03));
+		scene_1.newMushroom(getRandomFloat(0.2, 0.7), getRandomFloat(-2.0, 2.0), getRandomFloat(-2.0, 2.0), 0.00526354, 0.00999298, 0.0241513);
+		/*
+		red: 0.00526354
+		green: 0.00999298
+		blue: 0.0241513
+		*/
+		setup(scene_1.Mushrooms_ID.size() - 1);
 		glutPostRedisplay();
 		break;
 	case 's':
-		scene_1.Mushrooms_ID[0].showIndices();
+
+		if (guidesMode == 'a')
+		{
+			guidesMode = 'b';
+		}
+		else if(guidesMode == 'b')
+		{
+			guidesMode = 'a';
+		}
 		glutPostRedisplay();
 		break;
 	case 'p':
@@ -283,11 +437,11 @@ int main(int argc, char **argv)
 	// Register the callback function for non-ASCII key entry.
 	glutSpecialFunc(specialKeyInput);
 
+
+
 	glewExperimental = GL_TRUE;
 	glewInit();
-
-	
-
+	glClearColor(0.5, 0.5, 0.5, 0.0);
 	glutMainLoop();
 }
 // ----------------------------------------------------------------------------------------------------------------------------------------------------
