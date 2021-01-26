23052eb5238a6fbe383b23892ce3db1ba2423f67
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-25 22:44:45 -0300

    Update scene_class.cpp

--- source/scene_class.cpp
+++ source/scene_class.cpp
@@ -1,6 +1,6 @@
 #include "scene_class.h"
 #include "builder_class.h"
-
+#include <iostream>
 Scene::Scene(float X, float Y)
 {
 	setArea(X, Y);
@@ -12,9 +12,19 @@ void Scene::setArea(float x, float y)
 	area_y = y;
 }
 
-void Scene::newMushroom(float Radio, float X, float Y, float red, float green, float blue)
+float Scene::getArea_x()
 {
-	BuilderClass Shroom(Radio, X, Y, red, green, blue);
+	return area_x;
+}
+
+float Scene::getArea_y()
+{
+	return area_y;
+}
+
+void Scene::newMushroom(Strain id)
+{
+	BuilderClass Shroom(id);
 	Mushrooms_ID.push_back(Shroom);
 	total_entities++;
 }
