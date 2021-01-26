2ecb01706144b78a6fa160159527c28c4688fa90
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-25 22:43:33 -0300

    Update specie_class.cpp

--- source/specie_class.cpp
+++ source/specie_class.cpp
@@ -3,56 +3,80 @@
 using namespace std;
 #include "specie_class.h"
 
-Specie::Specie(string genre, string name, string location)
+Specie::Specie(int id)
 {
-    setGenre(genre);
-    setName(name);
-    setLocation(location);
+	selectSpecie(id);
 }
-void Specie::setGenre(string genre_a) {
-    if (genre_a.size() <= 16) {
-        genre = genre_a;
+void Specie::setFamily(string family_a) {
+    if (family_a.size() <= 30) {
+        family = family_a;
     }
     else {
-        genre = genre_a.substr(0, 25);
-        std::cerr << "El genero\"" << genre_a << "\" excede la longitud maxima (16). \n" << std::endl;
+		family = family_a.substr(0, 45);
+        std::cerr << "La familia\"" << family_a << "\" excede la longitud maxima (16). \n" << std::endl;
     }
 }
-
 void Specie::setName(string name_a) {
-    if (name_a.size() <= 16) {
+    if (name_a.size() <= 30) {
         name = name_a;
     }
     else {
-        name = name_a.substr(0, 25);
+        name = name_a.substr(0, 45);
         cerr << "El nombre\"" << name_a << "\" excede la longitud maxima (16). \n" << endl;
     }
 }
-
-void Specie::setLocation(string location_a) {
-    if (location_a.size() <= 16) {
-        location = location_a;
+void Specie::setBiome(string biome_a) {
+    if (biome_a.size() <= 30) {
+        biome = biome_a;
     }
     else {
-        location = location_a.substr(0, 25);
-        cerr << "La locacion\"" << location_a << "\" excede la longitud maxima (16). \n" << endl;
+		biome = biome_a.substr(0, 45);
+        cerr << "El bioma\"" << biome_a << "\" excede la longitud maxima (16). \n" << endl;
     }
 }
 
-string Specie::getGenre() const {
-    return genre;
+string Specie::getFamily() const {
+    return family;
 }
 string Specie::getName() const {
     return name;
 }
-
-string Specie::getLocation() const {
-    return location;
+string Specie::getBiome() const {
+    return biome;
 }
 
+void Specie::selectSpecie(int id)
+{
+	switch (id)
+	{
+	case(1):
+		setFamily("Bolbitiaceae");
+		setName("Panaeolus Cyanescens");
+		setBiome("Tropical");
+		break;
+	case(2):
+		setFamily("Strophariaceae");
+		setName("Kuehneromyces mutabilis");
+		setBiome("Forest");
+		break;
+	case(3):
+		setFamily("Amanitaceae");
+		setName("Amanita Muscaria");
+		setBiome("Boreal");
+		break;
+	case(4):
+		setFamily("Bolbitiaceae");
+		setName("Roridomyces Phyllostachys");
+		setBiome("Asian Forest");
+		break;
+	default:
+		std::cerr << "[!] INVALID SHROOM ID: (1 - 4)\n";
+		break;
+	}
+}
 void Specie::showInfo()const {
     cout << " ----------------------------------- SPECIE ----------------------------------- \n\n";
-    cout << " Genre: " << getGenre() << endl;
+    cout << " Genre: " << getFamily() << endl;
     cout << " Name: " << getName() << endl;
-    cout << " Location: " << getLocation() << endl << endl;
+    cout << " Location: " << getBiome() << endl << endl;
 }
