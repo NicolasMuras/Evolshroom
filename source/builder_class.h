b815838bf818d00429138afef948db53a1687b2f
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-01-25 22:42:30 -0300

    Update builder_class.h

--- source/builder_class.h
+++ source/builder_class.h
@@ -1,6 +1,7 @@
 #ifndef BUILDER_CLASS_H
 #define BUILDER_CLASS_H
 
+#include "strain_class.h"
 #include <vector>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
@@ -9,17 +10,63 @@ class BuilderClass
 {
 public:
 	
-	BuilderClass(float radio, float X, float Y, float r, float g, float b);
+	BuilderClass(Strain id);
+	~BuilderClass();
 
+	// MAIN FUNCTIONS
+	void buildStrain(Strain);
 	void buildShroom();
-	void showMushroom() const;
-	void setStage(int);
-	int getStage() const;
-	void showIndices();
-	std::vector<unsigned int> getIndices();
 
+	void showGuides() const;
+	void showBuilderInfo();
+
+	std::vector<unsigned int> getIndices();
+	std::vector<unsigned int> getCapIndices();
+
+	// Vertex data
+	float cap_group[12][20][3] = { 0 };
+	float cap_colors[12][20][3] = { 0 };
+	float circle_group[30][20][3] = { 0 };
+	float colors_group[30][20][3] = { 0 };
+
+private:
 	// Const vars
 	const int vertex_quantity = 20;
+
+	// Build functions
+	void buildTrunk();
+	void buildCapCircles(int&);
+	void buildCircle(float, float, float);
+	void buildCapCircle(int&, float, float);
+	void y_translateCap(float, int, float);
+	void x_translateCap(float, int, float);
+	void z_inclinationCap(float, int);
+
+	void generateIndices();
+	void generateIndicesCap();
+	void generateColors();
+	void generateColorsCap();
+
+	// SET
+	void setStage(int);
+	void setLocation(float, float, float);
+	void setTrunkColors(float, float, float);
+	void setCapColors(float, float, float);
+
+	// GET
+	int getStage() const;
+	int getRandomInt(int, int) const;
+	float getRandomFloat(float, float) const;
+
+	// VARS
+	float X_Loc = 0;
+	float Y_Loc = 0;
+	float Radio_Loc = 0;
+	float circle_inclination = 0;
+	float circle_translation = 0;
+	float trunk_curvation = 0;
+	float cap_curvation = 0;
+	int stage = 0;
 
 	// Procedural vars
 	int root_curvation_end = 5;
@@ -32,50 +79,17 @@ public:
 	int max_stage = 23;
 	float min_trunk_width = 5.5;
 	float cap_flatness = 0.05;
-	float red = 0.2;
-	float green = 0.2;
-	float blue = 0.2;
+	float red_cap = 0.2;
+	float green_cap = 0.2;
+	float blue_cap = 0.2;
+	float red_trunk = 0.2;
+	float green_trunk = 0.2;
+	float blue_trunk = 0.2;
 
 	// Vertex data
-	float cap_group[12][20][3] = { 0 };
-	float cap_colors[12][20][3] = { 0 };
 	float cap_radius[12] = { 0 };
-	float circle_group[30][20][3] = { 0 };
-	float colors_group[30][20][3] = { 0 };
 
 	std::vector<unsigned int> indices = { 1 };
 	std::vector<unsigned int> indices_cap = { 1 };
-
-private:
-	// Build functions
-	void buildTrunk();
-	void buildCapCircles(int&);
-	void buildCircle(float, float, float);
-	void buildCapCircle(int&, float, float);
-	int getRandomInt(int, int) const;
-	float getRandomFloat(int, int) const;
-	void y_translateCap(float, int, float);
-	void x_translateCap(float, int, float);
-	void z_inclinationCap(float, int);
-
-	void generateIndices();
-	void generateIndicesCap();
-	void generateColors();
-	void generateColorsCap();
-
-	// SET
-	void setLocation(float, float, float);
-	void setColors(float, float, float);
-	void setRandomVars();
-
-	// VARS
-	float X_Loc = 0;
-	float Y_Loc = 0;
-	float Radio_Loc = 0;
-	float circle_inclination = 0;
-	float circle_translation = 0;
-	float trunk_curvation = 0;
-	float cap_curvation = 0;
-	int stage = 0;
 };
 #endif // BUILDER_CLASS_H
